3eb5a3a2ec2345b8860004004cdb80307ab84bec
Michael Velasco <michsoftster@gmail.com>
2016-03-12 14:06:23 -0600

    Rotate

---
+++ ClassDemo/Assets/RotateAndColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAndColor : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+        StartCoroutine("RotateColor");
+	}
+
+    IEnumerator RotateColor()
+    {
+        yield return new WaitForSeconds(3f);
+        transform.Rotate(0,10,0);
+        GetComponent<MeshRenderer>().material.color = new Color(0,1,0,1);
+    }
+}
