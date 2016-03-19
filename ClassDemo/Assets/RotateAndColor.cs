d67996f6c7577573ae5aaf0326fa6165d79ace28
shiniwes <shiniwes@hotmail.com>
2016-03-18 23:16:22 -0600

    Cambio de rotación, color y tiempo de espera en cubo de escena ejemplo.

--- ClassDemo/Assets/RotateAndColor.cs
+++ ClassDemo/Assets/RotateAndColor.cs
@@ -12,8 +12,8 @@ public class RotateAndColor : MonoBehaviour
 
     IEnumerator RotateColor()
     {
-        yield return new WaitForSeconds(3f);
-        transform.Rotate(0,10,0);
-        GetComponent<MeshRenderer>().material.color = new Color(0,1,0,1);
+        yield return new WaitForSeconds(6.283185f);
+        transform.Rotate(45.0f,-10.0f,15.40f);
+        GetComponent<MeshRenderer>().material.color = new Color(255,128,0,1);
     }
 }
